e748799e7ece245385f0fd692177c86d533b00a1
Reza Nickmanesh <reznick.reznick@gmail.com>
2017-05-16 18:20:48 -0700

    Update Refine_ Code.R

--- Refine_ Code.R
+++ Refine_ Code.R
@@ -1,8 +1,9 @@
 
 #Clean up brand names
  #1st way
+refine$company <- as.character(refine$company)
 
-refine2 <- refine2 %>% mutate(company=ifelse(grepl("^ph|^fil|^phl", company, ignore.case=TRUE), "philips", company)) %>% 
+refine <- refine %>% mutate(company=ifelse(grepl("^ph|^fil|^phl", refine$company, ignore.case=TRUE), "philips", refine$company))%>% 
   mutate(company=ifelse(grepl("^van", company, ignore.case=TRUE), "van houten", company)) %>% 
   mutate(company=ifelse(grepl("^uni", company, ignore.case=TRUE), "unilever", company)) %>% 
   mutate(company=ifelse(grepl("^ak", company, ignore.case=TRUE), "akzo", company))
@@ -49,8 +50,12 @@ refine <- refine %>% unite("full address", address, city, country, sep=", ")
 #Company: company_philips, company_akzo, company_van_houten and company_unilever.
 refine <- refine %>% mutate(company_philips = ifelse(company =="philips", 1, 0)) %>%
   mutate(company_akzo = ifelse(company == "akzo", 1, 0)) %>%
-  mutate(company_van_houten = ifelse(company == "van_houten", 1, 0)) %>%
+  mutate(company_van_houten = ifelse(company == "van houten", 1, 0)) %>%
   mutate(company_unilever = ifelse(company == "unilever", 1, 0))
+
 #product category: product_smartphone, product_tv, product_laptop and product_tablet.
-refine <- refine %>% mutate(product_smartphone = ifelse(product== "Smartphone", 1, 0)) %>% mutate(product_TV = ifelse(product == "TV", 1, 0)) %>% mutate(product_laptop = ifelse(product == "Laptop", 1, 0)) %>% mutate(product_tablet = ifelse(product == "Tablet", 1, 0))
+refine <- refine %>% mutate(product_smartphone = ifelse(product_category== "smartphone", 1, 0)) %>% 
+  mutate(product_TV = ifelse(product_category == "TV", 1, 0)) %>% 
+  mutate(product_laptop = ifelse(product_category == "Laptop", 1, 0)) %>% 
+  mutate(product_tablet = ifelse(product_category == "Tablet", 1, 0))
 
